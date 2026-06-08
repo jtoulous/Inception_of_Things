9714258f252dfc5048294b58dbfe3c7c28ef7e2f
josh <josh@Debian.myguest.virtualbox.org>
2026-06-08 16:09:43 +0200

    wesh2

--- p3/requirements.sh
+++ p3/requirements.sh
@@ -1,27 +1,34 @@
 # DOCKER INSTALL
 if ! command -v docker &> /dev/null; then
-    sudo apt update
-    sudo apt install -y ca-certificates curl
-
-    if [ ! -f /etc/apt/keyrings/docker.asc ]; then
-        sudo install -m 0755 -d /etc/apt/keyrings
-        sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-        sudo chmod a+r /etc/apt/keyrings/docker.asc
-        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian trixie stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-        sudo apt update
-    fi
-
-    sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    apt update
+    apt install -y ca-certificates curl
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] \
+https://download.docker.com/linux/debian trixie stable" | \
+    tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt update
+    apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    echo "Docker installé"
+else
+    echo "Docker déjà installé, skip"
 fi
 
 # K3D INSTALL
 if ! command -v k3d &> /dev/null; then
     curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+    echo "K3d installé"
+else
+    echo "K3d déjà installé, skip"
 fi
 
 # KUBECTL INSTALL
 if ! command -v kubectl &> /dev/null; then
     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     chmod +x kubectl
-    sudo mv kubectl /usr/local/bin/kubectl
+    mv kubectl /usr/local/bin/kubectl
+    echo "Kubectl installé"
+else
+    echo "Kubectl déjà installé, skip"
 fi
